ea77a6e4f2f958f42aeef0cd4bbd6057c6f004de
BuildTools <unconfigured@null.spigotmc.org>
2024-11-12 13:35:02 +0100

    Initial Commit

---
+++ src/test/java/fr/avainfo/springauthenticationexample/SpringAuthenticationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.avainfo.springauthenticationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthenticationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
