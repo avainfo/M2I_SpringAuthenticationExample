84b9c25a51d17f8f962f1b389048014d2e21eef3
BuildTools <unconfigured@null.spigotmc.org>
2024-11-12 17:00:42 +0100

    v3

--- src/main/java/fr/avainfo/springauthenticationexample/security/SecurityConfig.java
+++ src/main/java/fr/avainfo/springauthenticationexample/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package fr.avainfo.springauthenticationexample.security;
 
-import fr.avainfo.springauthenticationexample.handlers.RoleAccessDeniedHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
